241bc7cc7d4256810f49322626ba4bff7011c017
Victor Huerlimann <huerly@MacBook-Pro-2.fritz.box>
2016-02-11 10:09:18 +0100

    ...

--- OpticalLayer/StatefulObjectFilter.cpp
+++ OpticalLayer/StatefulObjectFilter.cpp
@@ -173,8 +173,6 @@ void StatefulObjectFilter::computeMeanMotionCenters()
 		if (t->currentUpdateFirstIndex < 0 || t->relatedStates.size() < 2)
 			continue;
 
-		/*
-
 		double accum_displacementx = 0.0;
 		double accum_displacementy = 0.0;
 		double xmin = RAND_MAX;
@@ -206,7 +204,6 @@ void StatefulObjectFilter::computeMeanMotionCenters()
 		tmp.y = ymin + (ymax - ymin) / 2;
 
 		t->relatedStates.push_back(makeTrackedState(tmp));
-		*/
 
 		t->currentUpdateFirstIndex = -1;
 	}
