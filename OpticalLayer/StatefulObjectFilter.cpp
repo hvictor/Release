b2e2bd2e03fc49a7626b4906c78add1fd4df0206
Victor Huerlimann <huerly@MacBook-Pro-2.fritz.box>
2016-02-11 11:29:37 +0100

    ...

--- OpticalLayer/StatefulObjectFilter.cpp
+++ OpticalLayer/StatefulObjectFilter.cpp
@@ -143,7 +143,7 @@ StateRelatedTable *StatefulObjectFilter::relateStateToTable(TrackedState *state)
 	StateRelatedTable *stateTable = NULL;
 
 	for (int i = 0; i < tables.size(); i++) {
-		if (relatedPrecise(state, tables[i])) {
+		if (relatedDisplacement(state, tables[i])) {
 			stateTable = tables[i];
 			break;
 		}
