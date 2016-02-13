f7f1c45e661d5a2e686951d8d12f7f2f6173d06b
sled <victor.huerlimann@gmail.com>
2016-02-13 15:46:02 +0100

    Update

---
+++ InterpolationEngine/InterpolationEngine.cpp
@@ -0,0 +1,18 @@
+/*
+ * InterpolationEngine.cpp
+ *
+ *  Created on: Feb 13, 2016
+ *      Author: sled
+ */
+
+#include "InterpolationEngine.h"
+
+InterpolationEngine::InterpolationEngine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+InterpolationEngine::~InterpolationEngine() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ InterpolationEngine/InterpolationEngine.h
@@ -0,0 +1,17 @@
+/*
+ * InterpolationEngine.h
+ *
+ *  Created on: Feb 13, 2016
+ *      Author: sled
+ */
+
+#ifndef INTERPOLATIONENGINE_H_
+#define INTERPOLATIONENGINE_H_
+
+class InterpolationEngine {
+public:
+	InterpolationEngine();
+	virtual ~InterpolationEngine();
+};
+
+#endif /* INTERPOLATIONENGINE_H_ */
