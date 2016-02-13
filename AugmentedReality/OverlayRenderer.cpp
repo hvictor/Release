3b1821b57c7472d6755f0b450e4497deb8c7fc4b
sled <victor.huerlimann@gmail.com>
2016-02-13 15:45:04 +0100

    interpolation engine added

--- AugmentedReality/OverlayRenderer.cpp
+++ AugmentedReality/OverlayRenderer.cpp
@@ -27,10 +27,10 @@ OverlayRenderer::~OverlayRenderer() {
 void OverlayRenderer::renderTracker(Mat frame, Point p, int size)
 {
 	rectangle(frame, Point(p.x - size, p.y - size), Point(p.x + size, p.y + size), OVERLAY_COLOR_GREEN, 1);
-	line(frame, Point(p.x, p.y - size), Point(p.x, p.y - (size-3)), Scalar(0, 0, 255), 1);
-	line(frame, Point(p.x, p.y + size), Point(p.x, p.y + (size-3)), Scalar(0, 0, 255), 1);
-	line(frame, Point(p.x - size, p.y), Point(p.x - (size-3), p.y), Scalar(0, 0, 255), 1);
-	line(frame, Point(p.x + (size-3), p.y), Point(p.x + size, p.y), Scalar(0, 0, 255), 1);
+	line(frame, Point(p.x, p.y - size), Point(p.x, p.y - (size-3)), OVERLAY_COLOR_GREEN, 1);
+	line(frame, Point(p.x, p.y + size), Point(p.x, p.y + (size-3)), OVERLAY_COLOR_GREEN, 1);
+	line(frame, Point(p.x - size, p.y), Point(p.x - (size-3), p.y), OVERLAY_COLOR_GREEN, 1);
+	line(frame, Point(p.x + (size-3), p.y), Point(p.x + size, p.y), OVERLAY_COLOR_GREEN, 1);
 }
 
 void OverlayRenderer::renderTrackerState(Mat frame, StateRelatedTable *table, Point p)
