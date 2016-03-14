bcd6140b07cbff320f42ea91b917340c451500de
sled <victor.huerlimann@gmail.com>
2016-03-14 19:44:04 +0100

    asd

---
+++ TrackerRT_UI/UIModel.h
@@ -0,0 +1,42 @@
+#ifndef UIMODEL_H
+#define UIMODEL_H
+
+#include "../Configuration/Configuration.h"
+#include <QString>
+#include <QObject>
+
+class UIModel : public QObject
+{
+public:
+    static UIModel *getInstance();
+
+public slots:
+    // Frame Info
+    void setFrameWidth(QString& wStr);
+    void setFrameHeight(QString& hStr);
+    void setFrameChannels(int chans);
+    void setFrameOutputType(int type);
+
+    // Input Device
+    void setInputDevice(int inputDevice);
+
+    // Processing Mode
+    void setProcessingMode(int processingMode);
+
+    // DUO Device
+    void setDUOGain(int gainPercent);
+    void setDUOExposure(int exposurePercent);
+    void setDUOIRLedArrayIntensity(int irLEDIntensityPercent);
+
+    // Apply
+    void apply();
+
+private:
+    UIModel();
+    FrameInfo frameInfo;
+    OperationalMode operationalMode;
+    OpticalLayerParameters opticalLayerParam;
+    DUOStereoCameraHardwareParameters duoParam;
+};
+
+#endif // UIMODEL_H
