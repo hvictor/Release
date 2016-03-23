d12bc91734918571da652de2a2b6ad0d78f37ad3
sled <victor.huerlimann@gmail.com>
2016-03-23 15:06:39 +0100

    ui

---
+++ TrackerRT_UI/UIController.h
@@ -0,0 +1,14 @@
+#ifndef UICONTROLLER_H
+#define UICONTROLLER_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QWidget>
+
+class UIController
+{
+public:
+    UIController();
+};
+
+#endif // UICONTROLLER_H
