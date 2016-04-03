2f397d2f6c8443ae0f218a37c7a835eccea4af46
sled <victor.huerlimann@gmail.com>
2016-04-03 22:42:08 +0200

    Update

--- FastMemory/fast_mem_pool.cpp
+++ FastMemory/fast_mem_pool.cpp
@@ -18,30 +18,27 @@ static int frame_buffer_size;
 volatile int mem_count;
 static FrameData *mem_head;
 static FrameData *mem_tail;
-static FrameData **mem;
+static FrameData *mem;
 
 void fast_mem_pool_init(int frame_width, int frame_height, int channels)
 {
 	frame_buffer_size = Configuration::getInstance()->getOpticalLayerParameters().frameBufferSize;
 	pthread_spin_init(&mem_spin, 0);
 
-	mem = (FrameData **)malloc(frame_buffer_size * sizeof(FrameData *));
+	mem = (FrameData *)malloc(frame_buffer_size * sizeof(FrameData));
 
 	for (int i = 0 ; i < frame_buffer_size; i++) {
 		mem[i] = (FrameData *)malloc(sizeof(FrameData));
-		mem[i]->left_data = (uint8_t *)malloc(frame_width * frame_height * channels * sizeof(uint8_t));
+		mem[i].left_data = (uint8_t *)malloc(frame_width * frame_height * channels * sizeof(uint8_t));
 
 		if (Configuration::getInstance()->getOperationalMode().inputDevice != MonoCameraVirtual) {
-			mem[i]->right_data = (uint8_t *)malloc(frame_width * frame_height * channels * sizeof(uint8_t));
+			mem[i].right_data = (uint8_t *)malloc(frame_width * frame_height * channels * sizeof(uint8_t));
 		}
-
-		mem[i]->index = i;
-		mem[i]->free = 1;
 	}
 
 	mem_count = 0;
-	mem_head = *mem;
-	mem_tail = *mem;
+	mem_head = mem;
+	mem_tail = mem;
 
 	sem_init(&sem_empty, 0, frame_buffer_size);
 
@@ -60,9 +57,9 @@ FrameData *fast_mem_pool_fetch_memory(void)
 
 	ret = mem_head;
 
-	if (mem_head == mem[frame_buffer_size-1]) {
+	if (mem_head == &(mem[frame_buffer_size-1])) {
 		printf("setting mem_head = mem[0]\n");
-		mem_head = mem[0];
+		mem_head = &(mem[0]);
 		printf("OK: setting mem_head = mem[0]\n");
 	}
 	else {
@@ -88,8 +85,8 @@ void fast_mem_pool_release_memory(FrameData *pFrameData)
 	*mem_tail = *pFrameData;
 	printf("mem pushed\n");
 
-	if (mem_tail == mem[frame_buffer_size-1]) {
-		mem_tail = mem[0];
+	if (mem_tail == &(mem[frame_buffer_size-1])) {
+		mem_tail = &(mem[0]);
 	}
 	else {
 		mem_tail++;
