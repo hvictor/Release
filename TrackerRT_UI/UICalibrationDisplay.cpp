ffc70404589a38cdabfc7e95f01c46409d9096f9
sled <victor.huerlimann@gmail.com>
2016-04-19 22:11:23 +0200

    as

---
+++ TrackerRT_UI/UICalibrationDisplay.cpp
@@ -0,0 +1,22 @@
+#include "UICalibrationDisplay.h"
+#include "ui_UICalibrationDisplay.h"
+
+UICalibrationDisplay::UICalibrationDisplay(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::UICalibrationDisplay)
+{
+    ui->setupUi(this);
+
+    ui->lcdGPUDistance->setSegmentStyle(QLCDNumber::Flat);
+    ui->lcdGPUDistance->setStyleSheet("background-color: black; color: lightgreen;");
+
+    ui->lcdGPUSegments->setSegmentStyle(QLCDNumber::Flat);
+    ui->lcdGPUSegments->setStyleSheet("background-color: black; color: lightgreen;");
+
+    QObject::connect(ui->btnAcquireFrame, SIGNAL(clicked()), ui->monoDisplay, SLOT(render()));
+}
+
+UICalibrationDisplay::~UICalibrationDisplay()
+{
+    delete ui;
+}
