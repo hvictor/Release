2bc3a3c117e59a96bd474958023fdd7dd13769b4
sled <victor.huerlimann@gmail.com>
2016-04-24 20:54:06 +0200

    Update

--- TrackerRT_UI/GLWidget.cpp
+++ TrackerRT_UI/GLWidget.cpp
@@ -63,9 +63,16 @@ void GLWidget::mouseReleaseEvent(QMouseEvent *event)
     int height = abs(origin.y() - dst.y());
 
     HSVRange hsv_range = HSVManager::getInstance()->getHSVRange((const uint8_t *)u8data, 640, 480, left, top, width, height);
-    u8data = HSVManager::getInstance()->filterHSVRange((const uint8_t *)u8data, 640, 648, hsv_range).data;
+
+    printf("GLWidget :: mouseReleaseEvent :: Assigning data...\n");
+
+    this->u8data = HSVManager::getInstance()->filterHSVRange((const uint8_t *)u8data, 640, 648, hsv_range).data;
+
+    printf("GLWidget :: mouseReleaseEvent :: Data assigned, updating\n");
 
     update();
+
+    printf("GLWidget :: mouseReleaseEvent :: Updated\n");
     /*
     QRect selectionArea(left, top, width, height);
 
