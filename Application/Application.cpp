ef9f30202f5752cb43447bbcd1b24e8556d72b6a
sled <victor.huerlimann@gmail.com>
2016-05-01 23:54:10 +0200

    Update

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -226,8 +226,8 @@ void *frames_processor(void *)
 
 		//hsvManager->filterHSVRange(frame_data[0]->left_data, width, height, hsvRangeTGT, frame_data[0]->left_data);
 		hsvManager->filterHSVRange(frame_data[1]->left_data, width, height, hsvRangeTGT, frame_data[1]->left_data);
-		//hsvManager->filterHSVRange_out_8UC1(frame_data[1]->left_data, width, height, hsvRangeTGT, buf_8UC1_0);
-		f_8UC1_0 = hsvManager->filterHSVRange_out_Mat8UC1(frame_data[1]->left_data, width, height, hsvRangeTGT);
+		hsvManager->filterHSVRange_out_8UC1(frame_data[1]->left_data, width, height, hsvRangeTGT, buf_8UC1_0);
+		//f_8UC1_0 = hsvManager->filterHSVRange_out_Mat8UC1(frame_data[1]->left_data, width, height, hsvRangeTGT);
 
 		///////////////////////////////////////////////////////////////////////////////
 		//
@@ -340,8 +340,8 @@ void *frames_processor(void *)
 
 			statefulObjectFilter->tick();
 
-			//pred_scan_t engage_data = tgtPredator->engage_8UC1(buf_8UC1_0, width, height);
-			pred_scan_t engage_data = tgtPredator->engage_Mat8UC1(f_8UC1_0, width, height);
+			pred_scan_t engage_data = tgtPredator->engage_8UC1(buf_8UC1_0, width, height);
+			//pred_scan_t engage_data = tgtPredator->engage_Mat8UC1(f_8UC1_0, width, height);
 			circle(frame0_L, Point(engage_data.xl + (engage_data.xr-engage_data.xl)/2, engage_data.row), 4, Scalar(0, 0, 255), 1);
 			cout << "Predator: " << Point(engage_data.xl + (engage_data.xr-engage_data.xl)/2, engage_data.row) << endl;
 
--- TargetPredator/TargetPredator.cpp
+++ TargetPredator/TargetPredator.cpp
@@ -89,7 +89,7 @@ pred_scan_t TargetPredator::engage_8UC1(uint8_t *data, int width, int height)
 
 		for (int j = 0; j < width; j++) {
 			if (offset_buf[j] > 0) {
-				printf("MECOJONI (%d)\n", offset_buf[j]);
+				printf("== buf == (%d)\n", offset_buf[j]);
 				if (scan) {
 					scan_len++;
 				}
