fd51b3a6631f70298c7302cd8b1a4043c496bff6
sled <victor.huerlimann@gmail.com>
2016-05-05 23:49:33 +0200

    Update

--- AugmentedReality/OverlayRenderer.cpp
+++ AugmentedReality/OverlayRenderer.cpp
@@ -208,7 +208,7 @@ void OverlayRenderer::renderPredatorState(Mat frame, TargetPredator *tgtPredator
 
 			if (impact_status && k == 0) {
 				rectangle(frame, Point2f(x1 - 10.0, y1 - 10.0), Point2f(x1 + 10.0, y1 + 10.0), OVERLAY_COLOR_GREEN_RGBA, 2);
-				putText(frame, "[IMPACT]", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.8, OVERLAY_COLOR_GREEN_RGBA, 2, CV_AA);
+				putText(frame, "[IMPACT]", Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, OVERLAY_COLOR_GREEN_RGBA, 2, CV_AA);
 			}
 			else
 				rectangle(frame, Point2f(cx - 4.0, cy - 4.0), Point2f(cx + 4.0, cy + 4.0), color, 1);
