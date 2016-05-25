93eb94022861cbf3a1b3bb50922c57cf199a87b5
sled <victor.huerlimann@gmail.com>
2016-05-25 19:47:25 +0200

    Update

--- AugmentedReality/OverlayRenderer.cpp
+++ AugmentedReality/OverlayRenderer.cpp
@@ -476,7 +476,13 @@ void OverlayRenderer::renderTarget3DPosition(Mat frame, Point center, StereoSens
 	measure.y_mm /= 1000.0;
 	measure.z_mm /= 1000.0;
 
+	printf("dio\n");
+
 	sprintf(stateMessage, "T=[%d,%d] XYZ=[%.2f, %.2f, %.2f] CONFID=%.2f", center.x, center.y, measure.x_mm, measure.y_mm, measure.z_mm, confidence);
 	putText(frame, stateMessage, Point(10, 30), FONT_HERSHEY_SIMPLEX, 0.7, OVERLAY_COLOR_RED_RGBA, 2, CV_AA);
+
+	printf("gesu\n");
 	circle(frame, center, 5, OVERLAY_COLOR_RED_RGBA, -1);
+
+	printf("madonna\n");
 }
