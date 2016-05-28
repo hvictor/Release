e7de3b87cdf4c4cb294b2134a5e214f245692488
sled <victor.huerlimann@gmail.com>
2016-05-28 13:03:31 +0200

    Update

--- AugmentedReality/OverlayRenderer.cpp
+++ AugmentedReality/OverlayRenderer.cpp
@@ -488,7 +488,6 @@ void OverlayRenderer::renderTarget3DPosition(Mat frame, Point center, StereoSens
 	circle(frame, center, 5, OVERLAY_COLOR_RED_RGBA, -1);
 
 	FILE *fp = fopen("/tmp/data.txt", "a+");
-	char buf[300];
-	sprintf(buf, "%.2f %.2f %.2f\n", measure.x_mm * 0.001, measure.y_mm * 0.001, measure.z_mm * 0.001);
+	fprintf(fp, "%.2f %.2f %.2f\n", measure.x_mm * 0.001, measure.y_mm * 0.001, measure.z_mm * 0.001);
 	fclose(fp);
 }
