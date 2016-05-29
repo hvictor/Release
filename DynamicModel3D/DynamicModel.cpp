d8d86ab29459a75c7943496f714724a7f10ada9f
sled <victor.huerlimann@gmail.com>
2016-05-29 15:51:23 +0200

    Update

---
+++ DynamicModel3D/DynamicModel.cpp
@@ -0,0 +1,84 @@
+/*
+ * DynamicModel.cpp
+ *
+ *  Created on: May 28, 2016
+ *      Author: sled
+ */
+
+#include "DynamicModel.h"
+
+DynamicModel *DynamicModel::getInstance()
+{
+	static DynamicModel *inst = 0;
+
+	if (inst == 0) {
+		inst = new DynamicModel();
+	}
+
+	return inst;
+}
+
+DynamicModel::DynamicModel()
+{
+	kalmanFilter = new DynamicModel3D_KalmanFilter();
+	filterCallCounter = 0;
+	config = Configuration::getInstance();
+}
+
+DynamicModel::~DynamicModel()
+{
+}
+
+void DynamicModel::compute_dynamical_state_data(dyn_state_t *actual, dyn_state_t *prev)
+{
+	struct timespec *prev_t = &(prev->t);
+	struct timespec *act_t = &(actual->t);
+
+	double delta_ms = nanotimer_rt_ms_diff(prev_t, act_t);
+
+	printf("Dynamic Model :: compute_dynamical_state_data() :: delta t = %.2f [ms]", delta_ms);
+
+	// Velocity components [mm / ms]
+	actual->vx = (actual->pos.x - prev->pos.x) / delta_ms;
+	actual->vy = (actual->pos.y - prev->pos.y) / delta_ms;
+	actual->vz = (actual->pos.z - prev->pos.z) / delta_ms;
+
+	printf("Dynamic Model :: compute_dynamical_state_data() :: v = [%.2f, %.2f, %.2f] [mm / ms] = [%.2f, %.2f, %.2f] [m / s]", actual->vx, actual->vy, actual->vz,
+			actual->vx * 0.001, actual->vy * 0.001, actual->vz * 0.001);
+}
+
+void DynamicModel::recalc(Vector3D v, struct timespec t)
+{
+	if (config->dynamicModelParameters.useInputKalmanFilter) {
+		if (filterCallCounter == 0) {
+			printf("Dynamic Model :: Kalman Filter :: Initializing filter.\n");
+			kalmanFilter->initParameters(v.x, v.y, v.z);
+			filterCallCounter = 1;
+		}
+
+		printf("Dynamic Model :: Kalman Filter :: MEAS=[%.2f %.2f %.2f] ", v.x, v.y, v.z);
+
+		kalmanFilter->recalc(filterCallCounter++, v.x, v.y, v.z, &v.x, &v.y, &v.z);
+
+		printf("FILT=[%.2f %.2f %.2f]\n", v.x, v.y, v.z);
+	}
+
+	// Make new dynamical state
+	dyn_state_t *dyn = make_dynamical_state(v, t);
+
+	// Compute velocity and acceleration components based on previous state
+	compute_dynamical_state_data(dyn, *(state.begin()));
+
+	// Add dynamical state to the model
+	state.push_front(dyn);
+}
+
+dyn_state_t *DynamicModel::make_dynamical_state(Vector3D v, struct timespec t)
+{
+	dyn_state_t *dyn_state = new dyn_state_t();
+
+	dyn_state->pos = v;
+	dyn_state->t = t;
+
+	return dyn_state;
+}
---
+++ DynamicModel3D/DynamicModel.h
@@ -0,0 +1,54 @@
+/*
+ * DynamicModel.h
+ *
+ *  Created on: May 28, 2016
+ *      Author: sled
+ */
+
+#ifndef DYNAMICMODEL_H_
+#define DYNAMICMODEL_H_
+
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+using namespace cv;
+
+#include "DynamicModel3D.h"
+#include "../RealTime/nanotimer_rt.h"
+#include "../Configuration/Configuration.h"
+
+typedef struct
+{
+	Vector3D pos;		// Real-world position
+
+	double ax, ay, az;	// Accelerations
+	double vx, vy, vz;	// Velocities
+
+	struct timespec t;	// Instant
+
+	bool impacted;		// Impact status
+} dyn_state_t;
+
+class DynamicModel {
+public:
+	virtual ~DynamicModel();
+	void recalc(Vector3D v, struct timespec t);
+	static DynamicModel *getInstance();
+
+private:
+	DynamicModel();
+	list<dyn_state_t *> state;
+	DynamicModel3D_KalmanFilter *kalmanFilter;
+	int filterCallCounter;
+	Configuration *config;
+	dyn_state_t *make_dynamical_state(Vector3D v, struct timespec t);
+	void compute_dynamical_state_data(dyn_state_t *actual, dyn_state_t *prev);
+};
+
+#endif /* DYNAMICMODEL_H_ */
