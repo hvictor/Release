d376f5ab01269217e52e92123b487e0b796a5e4a
sled <victor.huerlimann@gmail.com>
2016-05-31 20:06:20 +0200

    Update

---
+++ StaticModel/GroundModel.cpp
@@ -0,0 +1,48 @@
+/*
+ * GroundModel.cpp
+ *
+ *  Created on: May 30, 2016
+ *      Author: sled
+ */
+
+#include "GroundModel.h"
+
+GroundModel *getInstance()
+{
+	static GroundModel *inst = 0;
+
+	if (inst == 0) {
+		inst = new GroundModel();
+	}
+
+	return inst;
+}
+
+GroundModel::GroundModel()
+{
+}
+
+GroundModel::~GroundModel()
+{
+}
+
+PlaneLinearModel GroundModel::computeGroundPlaneLinearModel(Point nearL, Point nearR, Point farL, Point farR)
+{
+	PlaneLinearModel groundPlaneLinearModel;
+
+	// Request the Stereo Sensor's Driver instance
+	printf("Ground Model :: Compute Ground Plane Linear Model :: Point NEAR, LEFT:\n");
+	ZEDStereoSensorDriver::getInstance()->repeatedDepthMeasure(nearL.x, nearL.y);
+
+	printf("Ground Model :: Compute Ground Plane Linear Model :: Point NEAR, RIGHT:\n");
+	ZEDStereoSensorDriver::getInstance()->repeatedDepthMeasure(nearR.x, nearR.y);
+
+	printf("Ground Model :: Compute Ground Plane Linear Model :: Point FAR, LEFT:\n");
+	ZEDStereoSensorDriver::getInstance()->repeatedDepthMeasure(farL.x, farL.y);
+
+	printf("Ground Model :: Compute Ground Plane Linear Model :: Point FAR, RIGHT:\n");
+	ZEDStereoSensorDriver::getInstance()->repeatedDepthMeasure(farR.x, farR.y);
+
+	return groundPlaneLinearModel;
+}
+
---
+++ StaticModel/GroundModel.h
@@ -0,0 +1,41 @@
+/*
+ * GroundModel.h
+ *
+ *  Created on: May 30, 2016
+ *      Author: sled
+ */
+
+#ifndef GROUNDMODEL_H_
+#define GROUNDMODEL_H_
+
+#include "../Configuration/Configuration.h"
+#include "../StereoVision/StereoVision.h"
+#include "../SensorAbstractionLayer/ZEDStereoSensorDriver.h"
+#include "../Common/opencv_headers.h"
+#include "../Common/data_types.h"
+
+using namespace std;
+using namespace cv;
+
+typedef struct
+{
+	// Plane equation
+
+	// Points of appoggio
+	Vector3D nearL, nearR;
+	Vector3D farL, farR;
+
+} PlaneLinearModel;
+
+class GroundModel {
+public:
+	static GroundModel *getInstance();
+	virtual ~GroundModel();
+	PlaneLinearModel computeGroundPlaneLinearModel(Point nearL, Point nearR, Point farL, Point farR);
+
+private:
+	GroundModel();
+
+};
+
+#endif /* GROUNDMODEL_H_ */
