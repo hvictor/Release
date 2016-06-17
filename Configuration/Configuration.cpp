29899ac17cf32fcb1bbd8a897f26594b934fab79
sled <victor.huerlimann@gmail.com>
2016-06-17 20:32:50 +0200

    Update

--- Configuration/Configuration.cpp
+++ Configuration/Configuration.cpp
@@ -300,6 +300,8 @@ void Configuration::loadConfigFile(string fileName)
 	fs["CalibrationData_TGT_Smax"] >> calibrationData.targetHSVRange.Smax;
 	fs["CalibrationData_TGT_Vmax"] >> calibrationData.targetHSVRange.Vmax;
 
+
+	TennisFieldStaticModel::getInstance()->getInstance()->setTennisFieldDelimiter(new TennisFieldDelimiter());
 	fs["CalibrationData_FLD_2D_NearL_X"] >> TennisFieldStaticModel::getInstance()->getTennisFieldDelimiter()->bottomLeft.x;
 	fs["CalibrationData_FLD_2D_NearL_Y"] >> TennisFieldStaticModel::getInstance()->getTennisFieldDelimiter()->bottomLeft.y;
 	fs["CalibrationData_FLD_2D_NearR_X"] >> TennisFieldStaticModel::getInstance()->getTennisFieldDelimiter()->bottomRight.x;
