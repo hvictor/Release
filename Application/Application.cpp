396144750a8c82e5b91945c8d1c346fdcac37880
sled <victor.huerlimann@gmail.com>
2016-06-21 23:11:11 +0200

    Update

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -552,7 +552,7 @@ void *frames_output(void *)
 
 			// Direct binary serialization
 			OverlayRenderer::getInstance()->renderFrameCounter_8UC4(frame_data->left_data, 640, 480, frame_data->frame_counter);
-			//serialize_frame_data(frame_data);
+			serialize_frame_data(frame_data);
 
 			/*
 			printf("Serialization time: elapsed: %.2f [ms], Count: %d, Pressure: %.2f%%\n", nanotimer_rt_ms_diff(&s, &t),
