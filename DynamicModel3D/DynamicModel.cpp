6129e8e985b91faa7e750b4fb95036b69a5ea3d0
sled <victor.huerlimann@gmail.com>
2016-07-21 23:26:39 +0200

    Update

--- DynamicModel3D/DynamicModel.cpp
+++ DynamicModel3D/DynamicModel.cpp
@@ -84,6 +84,7 @@ dyn_model_result_t DynamicModel::recalc(Vector3D v, struct timespec t)
 		kalmanFilter->recalc(filterCallCounter++, v.x, v.y, v.z, &v.x, &v.y, &v.z);
 
 		printf("FILT=[%.2f %.2f %.2f]\n", v.x, v.y, v.z);
+
 	}
 
 	// Make new dynamical state
